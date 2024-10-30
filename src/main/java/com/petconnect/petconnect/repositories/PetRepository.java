792c50c89faf8e49d74cf50758a421c883776e71
Davi <davigregorio.a@hotmail.com>
2024-10-30 10:44:00 -0300

    Pet CRUD

--- src/main/java/com/petconnect/petconnect/repositories/PetRepository.java
+++ src/main/java/com/petconnect/petconnect/repositories/PetRepository.java
@@ -3,6 +3,8 @@ package com.petconnect.petconnect.repositories;
 import com.petconnect.petconnect.Entities.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PetRepository extends JpaRepository<Pet, Long> {
+import java.util.List;
 
+public interface PetRepository extends JpaRepository<Pet, Long> {
+    List<Pet> findByUserId(Long userId);
 }
--- src/main/java/com/petconnect/petconnect/services/PetService.java
+++ src/main/java/com/petconnect/petconnect/services/PetService.java
@@ -2,12 +2,17 @@ package com.petconnect.petconnect.services;
 
 import com.petconnect.petconnect.Entities.Pet;
 import com.petconnect.petconnect.Entities.User;
+import com.petconnect.petconnect.Exceptions.PetNotFoundException;
+import com.petconnect.petconnect.Exceptions.UserUnauthorizedException;
 import com.petconnect.petconnect.dtos.CreatePetRequest;
 import com.petconnect.petconnect.enums.PetGender;
 import com.petconnect.petconnect.repositories.PetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PetService {
@@ -23,4 +28,56 @@ public class PetService {
             return petRepository.save(pet);
     }
 
+    public Pet findPetById(Long petId) {
+        Optional<Pet> pet = petRepository.findById(petId);
+        if(pet.isEmpty()) throw new PetNotFoundException();
+        return pet.get();
+    }
+
+    public  List<Pet> getUserPets(Long userId) {
+        List<Pet> userPets = petRepository.findByUserId(userId);
+        return userPets;
+    }
+
+    public Pet updatePet (Long petId, Pet updatedPetBody) {
+        User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Optional<Pet> petOption = petRepository.findById(petId);
+
+        if (petOption.isEmpty()) {
+            throw new PetNotFoundException();
+        }
+
+        Pet petEntity = petOption.get();
+
+        boolean loggedUserIsPetUser = loggedUser.getId().equals(petEntity.getUser().getId());
+        if (!loggedUserIsPetUser) {
+            throw new UserUnauthorizedException("This pet does not belong to logged user.");
+        }
+
+        petEntity.setName(updatedPetBody.getName());
+        petEntity.setGender(updatedPetBody.getGender());
+        petEntity.setBirthDate(updatedPetBody.getBirthDate());
+        petEntity.setRace(updatedPetBody.getRace());
+        petEntity.setSpecie(updatedPetBody.getSpecie());
+        petEntity.setVaccines(updatedPetBody.getVaccines());
+        petRepository.save(petEntity);
+
+        return petEntity;
+    }
+
+    public void deletePet(Long petId) {
+        User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        Pet petEntity = petRepository
+                .findById(petId)
+                .orElseThrow(PetNotFoundException::new);
+
+        boolean loggedUserIsPetUser = loggedUser.getId().equals(petEntity.getUser().getId());
+        if (!loggedUserIsPetUser) {
+            throw new UserUnauthorizedException("This pet does not belong to logged user.");
+        }
+
+        petRepository.deleteById(petId);
+    }
 }
